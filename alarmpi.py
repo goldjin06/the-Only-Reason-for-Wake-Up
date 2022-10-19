c192517cf4737edacaf102e98bbb72a2085a6e8b
goldjin06 <goldjin06@naver.com>
2022-10-19 17:59:30 +0900

    알람 함수화
    버튼

--- alarmpi.py
+++ alarmpi.py
@@ -1,5 +1,6 @@
 # 이 소스 복사해서 나중에 붙여넣을예정
 import RPi.GPIO as GPIO
+import time
 
 button_red = 9
 button_yellow =  10
@@ -21,44 +22,63 @@ GPIO.setwarnings(False)
 
 pwm = GPIO.PWM(piezzo_buzzer, 1)
 
-# if 어쩌구 저쩌구:
 
 
+# 여기부터 소스코드 ################################################################
 
+def playAirplane():
+    beat = 0.5
+    melody(mi, 1.5)
+    melody(re, 0.5)
+    melody(do, 1)
+    melody(re, 1)
+    for i in range(3):
+        melody(mi, 1)
+        time.sleep(0.01)
+    time.sleep(beat)
+    for i in range(3):
+        melody(re, 1)
+        time.sleep(0.01)
+    time.sleep(beat)
+    for i in range(3):
+        melody(mi, 1)
+        time.sleep(0.01) 
+    time.sleep(beat) # 떴다떴다비행기날아라날아라
+    melody(mi, 1.5)
+    melody(re, 0.5)
+    melody(do, 1)
+    melody(re, 1)
+    for i in range(3):
+        melody(mi, 1)
+        time.sleep(0.01)
+    time.sleep(beat)
+    for i in range(2):
+        melody(re, 1)
+        time.sleep(0.01)
+    melody(mi, 1)
+    melody(re, 1)
+    melody(do, 1)
+    time.sleep(beat * 3)
+    
 
-
-def music_1():
-    pwm.start(50)
-    pwm.ChangeFrequency(192)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(329)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(192)
-    time.sleep(0.36)
-
-    pwm.ChangeFrequency(329)
-    time.sleep(0.12)
-
-    pwm.ChangeFrequency(293)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(246)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(261)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(293)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(329)
-    time.sleep(0.5)
-
-    pwm.ChangeFrequency(262)
-    time.sleep(0.36)
-
-    pwm.ChangeFrequency(391)
-    time.sleep(0.12)
+def melody(name, beat):
+    pwm.start()
+    if name == 'do':
+        pwm.ChangeFrequency(262)
+    elif name == 're':
+        pwm.ChangeFrequency(294)
+    elif name == 'mi':
+        pwm.ChangeFrequency(330)
+    elif name == 'fa':
+        pwm.ChangeFrequency(349)
+    elif name == 'sol':
+        pwm.ChangeFrequency(392)
+    elif name == 'la':
+        pwm.ChangeFrequency(440)
+    elif name == 'si':
+        pwm.ChangeFrequency(494)
+    time.sleep(bear * 0.5)
     pwm.stop()
+
+
+playAirplane()
